5c9d385be29867da279e948b9406ecaff1b2acf9
DavisSS10 <suclle.davis10@gmail.com>
2021-11-09 19:00:05 -0500

    Agregando Role al back y modificando Persona

--- ONTIME/src/main/java/pe/edu/upc/spring/model/Persona.java
+++ ONTIME/src/main/java/pe/edu/upc/spring/model/Persona.java
@@ -1,88 +1,103 @@
 package pe.edu.upc.spring.model;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Persona")
-public class Persona implements Serializable{
+@Table(name = "Persona")
+public class Persona implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int idPersona;
-	
-	@Column(name="nombrePersona", nullable=false, length=15)
-	private String nombrePersona;
-	
-	@Column(name="contrasenaPersona", nullable=false, length=20)
-	private String contrasenaPersona;
-	
-	@Column(name="tipoPersona", nullable=false)
-	private char tipoPersona;
-	
-	@Column(name="sueldoPersona")
-	private float sueldoPersona;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idPersona;
+
+	@Column(name = "username", nullable = false, length = 15)
+	private String username;
+
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "id_persona")
+	private List<Role> roles;
+	
 	public Persona() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
 
-	public Persona(int idPersona, String nombrePersona, String contrasenaPersona, char tipoPersona,
-			float sueldoPersona) {
+	public Persona(int idPersona, String username, String password, List<Role> roles) {
 		super();
 		this.idPersona = idPersona;
-		this.nombrePersona = nombrePersona;
-		this.contrasenaPersona = contrasenaPersona;
-		this.tipoPersona = tipoPersona;
-		this.sueldoPersona = sueldoPersona;
+		this.username = username;
+		this.password = password;
+		this.roles = roles;
 	}
 
+	/**
+	 * @return the idPersona
+	 */
 	public int getIdPersona() {
 		return idPersona;
 	}
 
+	/**
+	 * @param idPersona the idPersona to set
+	 */
 	public void setIdPersona(int idPersona) {
 		this.idPersona = idPersona;
 	}
 
-	public String getNombrePersona() {
-		return nombrePersona;
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
 	}
 
-	public void setNombrePersona(String nombrePersona) {
-		this.nombrePersona = nombrePersona;
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
-	public String getContrasenaPersona() {
-		return contrasenaPersona;
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
 	}
 
-	public void setContrasenaPersona(String contrasenaPersona) {
-		this.contrasenaPersona = contrasenaPersona;
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
-	public char getTipoPersona() {
-		return tipoPersona;
+	/**
+	 * @return the roles
+	 */
+	public List<Role> getRoles() {
+		return roles;
 	}
 
-	public void setTipoPersona(char tipoPersona) {
-		this.tipoPersona = tipoPersona;
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
 	}
 
-	public float getSueldoPersona() {
-		return sueldoPersona;
-	}
-
-	public void setSueldoPersona(float sueldoPersona) {
-		this.sueldoPersona = sueldoPersona;
-	}
-	
 }
