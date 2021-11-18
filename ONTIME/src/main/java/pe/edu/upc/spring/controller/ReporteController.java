8943d77a24935794f830ca6a9ab5731db0eb05a4
DavisSS10 <suclle.davis10@gmail.com>
2021-11-17 20:54:18 -0500

    Cambiando la tabla reporte por gráfico

--- ONTIME/src/main/java/pe/edu/upc/spring/controller/ReporteController.java
+++ ONTIME/src/main/java/pe/edu/upc/spring/controller/ReporteController.java
@@ -1,6 +1,7 @@
 package pe.edu.upc.spring.controller;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -22,7 +23,14 @@ public class ReporteController {
 	
 	@RequestMapping("/listar")
 	public String listarStatusPendientes(Map<String, Object> model) {
+		Map<String, Integer> graphData = new TreeMap<>();	
+		
+		for(int i = 0; i < rService.ListarStatusPendientes().size(); i++) {
+			graphData.put(rService.ListarStatusPendientes().get(i).getName(), rService.ListarStatusPendientes().get(i).getAmount());
+		}		
+		
 		model.put("reportes", rService.ListarStatusPendientes());
+		model.put("chartData", graphData);
 		model.put("reporte", new Reporte());
 		return "reportes"; 
 	}		
