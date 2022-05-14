d4aa64eac3257daa079fe8a4a7e3a304337d2a30
Andres <u20191229@upc.edu.pe>
2022-05-14 13:12:52 -0500

    prueba
    1

--- ONTIME/src/main/java/pe/edu/upc/spring/auth/handler/LoginSuccessHandler.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.spring.auth.handler;
-
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-import org.springframework.stereotype.Component;
-
-@Component
-public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-
-}
--- ONTIME/src/main/java/pe/edu/upc/spring/service/IReporteService.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.spring.service;
-
-import java.util.List;
-
-import pe.edu.upc.spring.model.Reporte;
-
-public interface IReporteService {
-	public List<Reporte> ListarStatusPendientes();
-}
